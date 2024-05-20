9ab6e37d31fd01ce462239d09c2b2000a5ce67f2
Elijah Matar <m.matar515@gmail.com>
2024-05-20 17:34:37 -0500

    removed class comments from lessons that did not reflect new logic

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,13 +1,8 @@
-// add comment func
 const createComment = async (event) => {
   event.preventDefault();
   if (event.target.matches('button')) {
-    // console.dir(event.target);
-
     const description = event.target.parentElement[0].value.trim();
     const blog_id = event.target.parentElement[0].dataset.id;
-
-    // console.log(description, blog_id)
 
     if (description && blog_id) {
       const response = await fetch('api/comments', {
@@ -26,7 +21,6 @@ const createComment = async (event) => {
 };
 
 const commentEl = document.querySelectorAll('.comment-forum');
-// console.log(commentEl)
 commentEl.forEach((comment) => {
   comment.addEventListener('click', createComment);
 });
