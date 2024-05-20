aa1640a93d241252659c387af5bffa15e9a60d3b
Elijah Matar <m.matar515@gmail.com>
2024-05-20 13:56:54 -0500

    delete undeeded utils functions from class lesson files

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,41 +1,58 @@
 const router = require('express').Router();
 const { Blog } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-router.post('/', async (req, res) => {
+// Create a blog
+router.post('/', withAuth, async (req, res) => {
   try {
-    const newBlog = await Blog.create({
+    const blogData = await Blog.create({
       ...req.body,
       user_id: req.session.user_id,
     });
-
-    res.status(200).json(newBlog);
-  } catch (err) {
+    res.status(200).json(blogData);
+  } catch (error) {
     res.status(500).json({
       status: 'error',
-      message: 'Sorry, a server error has occured'
+      message: 'Oops, an error has occurred',
     });
   }
 });
-
-router.delete('/:id', async (req, res) => {
+// Update a blog
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const blogData = await Blog.update(
+      {
+        title: req.body.title,
+        body: req.body.body,
+      },
+      {
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      },
+    );
+    res.status(200).json(blogData);
+  } catch (error) {
+    res.status(500).json({
+      status: 'error',
+      message: 'Oops, an error has occurred',
+    });
+  }
+});
+// Delete a blog
+router.delete('/:id', withAuth, async (req, res) => {
   try {
     const blogData = await Blog.destroy({
       where: {
         id: req.params.id,
-        user_id: req.session.user_id,
       },
     });
-
-    if (!blogData) {
-      res.status(404).json({ message: 'No blog found with this id!' });
-      return;
-    }
-
     res.status(200).json(blogData);
-  } catch (err) {
+  } catch (error) {
     res.status(500).json({
       status: 'error',
-      message: 'Sorry, a server error has occured'
+      message: 'Oops, an error has occurred',
     });
   }
 });
