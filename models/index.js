645c77b47ff8296cb9b2f1bbaa586c26af3b5f03
Elijah Matar <m.matar515@gmail.com>
2024-05-20 13:09:26 -0500

    referencing file for all models

--- models/index.js
+++ models/index.js
@@ -1,13 +1,33 @@
 const User = require('./User');
 const Blog = require('./Blog');
+const Comment = require('./Comment');
 
 User.hasMany(Blog, {
   foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+  onDelete: 'CASCADE',
 });
 
 Blog.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
+});
+
+Blog.hasMany(Comment, {
+  foreignKey: 'blog_id',
+  onDelete: 'CASCADE',
+});
+
+Comment.belongsTo(Blog, {
+  foreignKey: 'blog_id',
+});
+
+//User has many comments
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+});
+
+//Comment belongs to one User
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
 });
 
 module.exports = { User, Blog };
