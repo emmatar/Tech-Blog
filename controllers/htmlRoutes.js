ee65ad5c9976d9e3baa1ae77fc055893a7b00d1b
Elijah Matar <m.matar515@gmail.com>
2024-05-20 17:29:17 -0500

    homeroute included attr names for get('/') changed

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -1,14 +1,16 @@
+// ROUTES FOR HTML TEMPLATES
 const router = require('express').Router();
 const { User, Blog, Comment } = require('../models');
 const withAuth = require('../utils/auth');
-// Display/render homepage
+
+// Route to get main HTML page
 router.get('/', async (req, res) => {
   try {
     const blogData = await Blog.findAll({
       include: [
         {
           model: Comment,
-          attributes: ['id', 'user_id', 'description'],
+          attributes: ['description', 'id', 'user_id'],
           include: [
             {
               model: User,
@@ -18,72 +20,65 @@ router.get('/', async (req, res) => {
         },
       ],
     });
-    const blogs = blogData.map((blog) => blog.get({ plain: true }));
 
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
     res.render('homepage', {
       blogs,
       logged_in: req.session.logged_in,
     });
   } catch (err) {
-    res.status(500).json({
-      status: 'error',
-      message: 'Sorry, a server error has occured',
-    });
+    res.status(500).json(err);
   }
 });
-
-router.get('/blog/:id', withAuth, async (req, res) => {
-  try {
-    const blogData = await Blog.findByPk(req.params.id);
-
-    const blogs = blogData.get({ plain: true });
-    res.render('blog', {
-      ...blogs,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: 'error',
-      message: 'Sorry, a server error has occured',
-    });
+// render login page
+router.get('/login', async (req, res) => {
+  if (req.session.logged_in) {
+    return res.redirect('/');
   }
+  res.render('login');
 });
-
+// render dashboard page
 router.get('/dashboard', withAuth, async (req, res) => {
   try {
     const userData = await User.findByPk(req.session.user_id, {
       include: [
         {
           model: Blog,
-          attributes: ['id', 'user_id', 'header', 'description'],
+          attributes: ['title', 'body', 'id', 'user_id'],
         },
       ],
     });
-
     const user = userData.get({ plain: true });
 
     res.render('dashboard', {
       ...user,
       logged_in: req.session.logged_in,
     });
-  } catch (err) {
+  } catch (error) {
     res.status(500).json({
-      status: err,
-      message: 'Sorry, a server error has occured',
+      status: 'error',
+      message: 'Oops, a server error!',
     });
   }
 });
-
 // render signup page
 router.get('/signup', async (req, res) => {
   res.render('signup');
 });
 
-router.get('/login', async (req, res) => {
-  if (req.session.logged_in) {
-    return res.redirect('/');
+// get blog by id
+router.get('/blogs/:id', withAuth, async (req, res) => {
+  try {
+    const blogData = await Blog.findByPk(req.params.id);
+
+    const blog = blogData.get({ plain: true });
+    res.render('blog', {
+      blog,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
   }
-  res.render('login');
 });
 
 module.exports = router;
