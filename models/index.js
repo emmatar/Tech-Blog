0432248b5646d774a4d837482e4fbbd850c920b0
Elijah Matar <m.matar515@gmail.com>
2024-05-20 17:33:13 -0500

    new comments on index for referencing models

--- models/index.js
+++ models/index.js
@@ -2,20 +2,23 @@ const User = require('./User');
 const Blog = require('./BlogPost');
 const Comment = require('./Comment');
 
+//User has many blogs
 User.hasMany(Blog, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE',
 });
 
+//Blog belong to one User
 Blog.belongsTo(User, {
   foreignKey: 'user_id',
 });
-
+//User has many blogs
 Blog.hasMany(Comment, {
   foreignKey: 'blog_id',
   onDelete: 'CASCADE',
 });
 
+//Blog belong to one User
 Comment.belongsTo(Blog, {
   foreignKey: 'blog_id',
 });
