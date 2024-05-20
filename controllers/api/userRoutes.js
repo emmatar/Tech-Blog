cbdcf0f6cec9608b68e49979ba74ce320b9d6b36
Elijah Matar <m.matar515@gmail.com>
2024-05-20 17:30:50 -0500

    updated comments on userRoutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// New user created
+// Create new User
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
@@ -13,48 +13,59 @@ router.post('/', async (req, res) => {
       res.status(200).json(userData);
     });
   } catch (err) {
-    res.status(500).json(err);
+    res.status(400).json({
+      status: 'error',
+      message: 'Oops, a server error',
+    });
   }
 });
 
+// Login User
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({
-      where: {
-        email: req.body.email,
-      },
+      where: { username: req.body.username },
     });
+
     if (!userData) {
       res
         .status(400)
         .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
-    const validPassword = await userData.checkPassword(req.body.password);
+
+    const validPassword = userData.checkPassword(req.body.password);
+
     if (!validPassword) {
       res
         .status(400)
         .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
+
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
 
-      res.json({ user: userData, message: 'You are now logged in!' });
+      // res.json({ user: userData, message: "You are now logged in!" });
+      res.redirect('/dashboard');
     });
   } catch (err) {
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 });
 
+// Logout User
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
       res.status(204).end();
     });
   } else {
-    res.status(404).end();
+    res.status(400).json({
+      status: 'error',
+      message: 'Oops, a server error',
+    });
   }
 });
 
