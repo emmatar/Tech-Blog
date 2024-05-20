7ba0f32990a371dcb552b042d28606fed466b9a7
Elijah Matar <m.matar515@gmail.com>
2024-05-20 15:27:56 -0500

    blog logic for corresponding handlebar

---
+++ public/js/blog.js
@@ -0,0 +1,46 @@
+const updateFormHandler = async (event) => {
+  event.preventDefault();
+
+  console.log(event.target.dataset.id);
+  const title = document.querySelector('#update-title').value.trim();
+  const body = document.querySelector('#update-body').value.trim();
+
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+    const response = await fetch(`/api/blogs/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({ title, body }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to update blog');
+    }
+  }
+};
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/blogs/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete blog');
+    }
+  }
+};
+
+document
+  .querySelector('#updateForm')
+  .addEventListener('submit', updateFormHandler);
+
+document
+  .querySelector('#delete-blog')
+  .addEventListener('click', delButtonHandler);
