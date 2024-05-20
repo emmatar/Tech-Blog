9b506f50e6c4b744c34a210a138d8b0fe6967aef
Elijah Matar <m.matar515@gmail.com>
2024-05-20 17:30:33 -0500

    added new coment routes for new comment model

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const commentRoutes = require('./commentRoutes');
 const blogRoutes = require('./blogRoutes');
 
 router.use('/users', userRoutes);
 router.use('/blogs', blogRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
